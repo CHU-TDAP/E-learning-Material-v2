033a487e8a0d8971b71195ef756951af71d31765
Yuan Chiu <chyuaner@gmail.com>
2015-02-09 19:27:46 +0800

    init grunt & package

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        watch: {
+            livereload: {
+                files: ['*.html', '**/*.css'],
+                tasks: ['copy', 'ssi'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    hostname: '*',
+                    livereload: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', []);
+    grunt.registerTask('default', ['build','connect', 'watch']);
+};
