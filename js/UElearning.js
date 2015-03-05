48e6f0d92e72924778dbfec64e74aa9ff2b7ab15
Yuan Chiu <chyuaner@gmail.com>
2015-03-05 17:31:43 +0800

    改良js/UElearning.View & JS檔案整理

--- js/UElearning.js
+++ js/UElearning.js
@@ -1,20 +1,26 @@
 var UElearning = {
     View: function(data, layout_data) {
+        var isEnableFinish = true;
+
         this.finishButton = layout_data.finishButton;
 
         this.minTime = data.minTime;
         this.minTimeForce = data.minTimeForce;
+        this.isEnableFinish = function() {
+            return isEnableFinish;
+        }
 
         this.alertInfo = function() {
             alert('minTime: '+this.minTime+'\n'+
                   'minTimeForce: '+this.minTimeForce);
         };
 
-        this.startAndroid = function() {
+        this.start = function() {
             var startTime = Date.now();
             var nowTime = Date.now();
             var elapsedTime = 0;
             var minTime = this.minTime;
+            var minTimeForce = this.minTimeForce;
             var remainderHold = minTime;
 
             var finishButton = this.finishButton;
@@ -28,18 +34,32 @@ var UElearning = {
                 remainderHold = minTime - elapsedTime;
 
                 if(remainderHold > 0) {
-                    if(!finishButton.classList.contains('disable')) {
-                        finishButton.classList.add('disable');
+                    if(minTimeForce) {
+                        isEnableFinish = false;
+                        if(!finishButton.classList.contains('disable')) {
+                            finishButton.classList.add('disable');
+                        }
+                        finishButton.href = '#';
                     }
-                    finishButton.href = '#';
                     finishButton.text = finishButtonText + ' ('+ remainderHold +')';
                 }
                 else {
+                    isEnableFinish = true;
                     if(finishButton.classList.contains('disable')) {
                         finishButton.classList.remove('disable');
                     }
                     finishButton.href = finishButtonHref;
                     finishButton.text = finishButtonText;
+                }
+            }
+        };
+
+        this.startAndroid = function() {
+            this.start();
+            var finishButton = this.finishButton;
+            finishButton.onclick = function() {
+                if(isEnableFinish) {
+                    Android.pressFinishButton();
                 }
             }
         };
